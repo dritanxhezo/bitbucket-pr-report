657eadc59edf9ab4257d4ecf950e700fcdfb4720
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2016-05-21 14:29:53 +0200

    Formatted code

--- src/main/java/net/ngeor/bprr/BitbucketClientImpl.java
+++ src/main/java/net/ngeor/bprr/BitbucketClientImpl.java
@@ -6,12 +6,10 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class BitbucketClientImpl implements BitbucketClient {
     private final HttpClientFactory httpClientFactory;
@@ -74,7 +72,7 @@ public class BitbucketClientImpl implements BitbucketClient {
 
     private void safeClose(Object x) {
         if (x instanceof Closeable) {
-            IOUtils.closeQuietly((Closeable)x);
+            IOUtils.closeQuietly((Closeable) x);
         }
     }
 }
